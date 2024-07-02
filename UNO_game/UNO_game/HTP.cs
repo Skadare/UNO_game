c592b94225d2004236b30b7cdcd24fc1bbe0e9cc
MarijaIsailovska <marija.isailovska2003@gmail.com>
2024-07-02 18:49:09 +0200

    Added HTP menu

--- UNO_game/UNO_game/HTP.cs
+++ UNO_game/UNO_game/HTP.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,24 @@ namespace UNO_game
         public HTP()
         {
             InitializeComponent();
+            pb1.BackgroundImage = Image.FromFile(Path.Combine(Application.StartupPath, "HTP", "How_to_play1.png"));
+            pbBack.Image = Image.FromFile(Path.Combine(Application.StartupPath, "HTP", "back3.png"));
+            pbBack.BringToFront();
         }
 
         private void HTP_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }

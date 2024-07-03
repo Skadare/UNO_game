77b86bcb294ba23311d5e937a49d58a9c06b489a
Darko Petrushevski <petrusevskidare@gmail.com>
2024-07-03 18:51:42 +0200

    Added Color Picker and Adding Two/Four cards

---
+++ UNO_game/UNO_game/Color_Picker.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace UNO_game
+{
+    partial class Color_Picker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardPB = new System.Windows.Forms.PictureBox();
+            this.redPB = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardPB
+            // 
+            this.cardPB.Location = new System.Drawing.Point(0, -3);
+            this.cardPB.Name = "cardPB";
+            this.cardPB.Size = new System.Drawing.Size(293, 396);
+            this.cardPB.TabIndex = 0;
+            this.cardPB.TabStop = false;
+            // 
+            // redPB
+            // 
+            this.redPB.Location = new System.Drawing.Point(114, 195);
+            this.redPB.Name = "redPB";
+            this.redPB.Size = new System.Drawing.Size(51, 75);
+            this.redPB.TabIndex = 1;
+            this.redPB.TabStop = false;
+            this.redPB.Click += new System.EventHandler(this.redPB_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(114, 102);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 87);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(161, 166);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(82, 47);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(33, 166);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(82, 47);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Color_Picker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 386);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.redPB);
+            this.Controls.Add(this.cardPB);
+            this.Name = "Color_Picker";
+            this.Text = "Color_Picker";
+            this.Load += new System.EventHandler(this.Color_Picker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cardPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardPB;
+        private System.Windows.Forms.PictureBox redPB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ UNO_game/UNO_game/Color_Picker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UNO_game
+{
+    public partial class Color_Picker : Form
+    {
+        public Boja selectedColor;
+        public Color_Picker()
+        {
+            InitializeComponent();
+            //293; 430
+            loadCard();
+
+            
+
+        }
+
+        private void Color_Picker_Load(object sender, EventArgs e)
+        {
+            //283x396
+        }
+        public void loadCard() {
+            /// za doma kje go sredim
+            cardPB.BackgroundImage = Image.FromFile(Path.Combine(Application.StartupPath, "Color_Picker", "wild_color_changer.png"));
+        }
+
+        private void cardPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void redPB_Click(object sender, EventArgs e)
+        {
+            selectedColor = Boja.RED;
+            this.Close();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
